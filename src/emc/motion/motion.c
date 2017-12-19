337cc295bb8f31688f4db647ee49e38ec411f865
Dewey Garrett <dgarrett@panix.com>
2017-12-19 08:27:48 -0700

    testing with default planner set to limit3_planner
    This is just a test for buildbot machines to identify
    problems
    
    Note: some limit3 tests are SKIPPED pending resolution of
          known issues noted in 2.7 limit3.comp using:
    
          tests/limit3.0/skip
          tests/limit3.1/skip
          tests/limit3.2/skip

--- src/emc/motion/motion.c
+++ src/emc/motion/motion.c
@@ -328,7 +328,7 @@ static int init_hal_io(void)
 
     /* simple_tp_planner selector -- for testing */
     if ((retval = hal_pin_u32_newf(HAL_IN, &(emcmot_hal_data->simple_tp_method), mot_comp_id, "motion.simple-tp-method")) != 0) goto error;
-    *(emcmot_hal_data->simple_tp_method) = 0; // select simple_tp planner
+    *(emcmot_hal_data->simple_tp_method) = 1; // TESTING select limit3_planner
 
     // spindle orient pins
     if ((retval = hal_pin_float_newf(HAL_OUT, &(emcmot_hal_data->spindle_orient_angle), mot_comp_id, "motion.spindle-orient-angle")) < 0) goto error;
